f39a6e7de20de954d8fdb356aea2201ddd6281cb
guguguwenjie <guguguwenjie@163.com>
2020-05-07 17:42:24 +0800

    weapp login

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Requests\Api\WeappAuthorizationRequest;
 use App\Models\User;
+use EasyWeChatComposer\EasyWeChat;
 use Illuminate\Support\Arr;
 use Illuminate\Http\Request;
 use Illuminate\Auth\AuthenticationException;
@@ -26,6 +28,50 @@ class AuthorizationsController extends Controller
         }
 
         return $this->respondWithToken($token)->setStatusCode(201);
+    }
+
+    public function weappStore(WeappAuthorizationRequest $request)
+    {
+		$code = $request->code;
+	    // 根据 code 获取微信 openid 和 session_key
+	    $miniProgram = \EasyWeChat::miniProgram();
+	    $data = $miniProgram->auth->session($code);
+
+	    // 如果结果错误，说明 code 已过期或不正确，返回 401 错误
+	    if (isset($data['errcode'])) {
+		    throw new AuthenticationException('code 不正确');
+	    }
+		//找到openid对应的用户
+	    $user = User::where('weapp_openid',$data['openid'])->first();
+	    $attributes['weixin_session_key'] = $data['session_key'];
+
+	    // 未找到对应用户则需要提交用户名密码进行用户绑定
+	    if(!$user){
+		    // 未找到对应用户则需要提交用户名密码进行用户绑定
+		    if(!$request->username){
+		    	throw new AuthenticationException('用户不存在');
+		    }
+		    $username = $request->username;
+		    filter_var($username,FILTER_VALIDATE_EMAIL) ?  $credentials['email'] = $username :  $credentials['phone'] = $username;
+		    $credentials['password'] = $request->password;
+
+		    // 验证用户名和密码是否正确
+		    if (!auth('api')->once($credentials)) {
+			    throw new AuthenticationException('用户名或密码错误');
+		    }
+
+		    // 获取对应的用户
+		    $user = auth('api')->getUser();
+		    $attributes['weapp_openid'] = $data['openid'];
+	    }
+
+	    // 更新用户数据
+	    $user->update($attributes);
+
+	    // 为对应用户创建 JWT
+	    $token = auth('api')->login($user);
+
+	    return $this->respondWithToken($token)->setStatusCode(201);
     }
 
     public function socialStore($type, SocialAuthorizationRequest $request)
